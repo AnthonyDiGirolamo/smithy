b6b3f0931e09b245be5280bc408ba7fc806a3477
Anthony DiGirolamo <anthony.digirolamo@gmail.com>
2012-01-25 14:54:35 -0500

    Refactored arch and swroot error checking

--- lib/smithy/util.rb
+++ lib/smithy/util.rb
@@ -6,11 +6,20 @@ class Object
 	end
 	remove_method :try
 	alias_method :try, :__send__
+
+  def blank?
+    respond_to?(:empty?) ? empty? : !self
+  end
 end
+
 class NilClass #:nodoc:
 	def try(*args)
 		nil
 	end
+
+  def blank?
+    true
+  end
 end
 
 module Smithy
@@ -30,4 +39,37 @@ module Smithy
 			stderr.replace("")
 		end
 	end
+
+  def load_system_config
+    sysconfig_path = File.expand_path(File.join(@smithy_bin_root,@smithy_config_file))
+    if File.exists? sysconfig_path
+      @smithy_config_hash = YAML.load_file(sysconfig_path)
+    else
+      STDERR.puts "warning: Cannot read config file: #{sysconfig_path}"
+    end
+  end
+
+  def get_arch(args = {})
+    @hostname = `hostname`.chomp
+    return args[:command_line_arch] unless args[:command_line_arch].nil?
+
+    if @hostname =~ /(\D*)(\d*)/
+      machine = $1
+      arch = @smithy_config_hash.try(:[], :"hostname-architectures").try(:[], machine)
+      return arch
+    end
+    return nil
+  end
+
+  def get_software_root(args = {})
+    if args[:root].blank? || args[:arch].blank?
+      raise """Cannot determine which architecture to build for.
+       Please specify using --arch or add a '#{@hostname}' hostname entry to:
+       #{@smithy_bin_root}/#{@smithy_config_file}"""
+    end
+
+    swroot = File.join(args[:root], args[:arch])
+    raise "The software-root directory '#{swroot}' is not valid" unless Dir.exist?(swroot)
+    return swroot
+  end
 end
