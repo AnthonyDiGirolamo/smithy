33bd90d27137b9a9e4e96ab5a658bfc9dc23e5fc
Anthony DiGirolamo <anthony.digirolamo@gmail.com>
2012-02-03 17:18:34 -0500

    Added preliminary bash and zsh completion files

--- etc/autocomplete.bash
+++ etc/autocomplete.bash
@@ -1,9 +1,173 @@
-complete -F get_smith_targets smith
-function get_smith_targets()
+if [[ -n ${ZSH_VERSION-} ]]; then
+  autoload -U +X bashcompinit && bashcompinit
+fi
+
+__smithycomp_words_include ()
 {
-  if [ -z $2 ] ; then
-    COMPREPLY=(`smith help -c`)
-  else
-    COMPREPLY=(`smith help -c $2`)
-  fi
+	local i=1
+	while [[ $i -lt $COMP_CWORD ]]; do
+		if [[ "${COMP_WORDS[i]}" = "$1" ]]; then
+			return 0
+		fi
+		i=$((++i))
+	done
+	return 1
 }
+
+# Find the previous non-switch word
+__smithycomp_prev ()
+{
+	local idx=$((COMP_CWORD - 1))
+	local prv="${COMP_WORDS[idx]}"
+	while [[ $prv == -* ]]; do
+		idx=$((--idx))
+		prv="${COMP_WORDS[idx]}"
+	done
+}
+
+
+__smithycomp ()
+{
+	# break $1 on space, tab, and newline characters,
+	# and turn it into a newline separated list of words
+	local list s sep=$'\n' IFS=$' '$'\t'$'\n'
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+
+	for s in $1; do
+		__smithycomp_words_include "$s" && continue
+		list="$list$s$sep"
+	done
+
+	IFS=$sep
+	COMPREPLY=($(compgen -W "$list" -- "$cur"))
+}
+
+_smithy ()
+{
+  #echo "cur: $cur, prev: $prev" > /dev/pts/33
+
+	local i=1 cmd
+
+	# find the subcommand
+	while [[ $i -lt $COMP_CWORD ]]; do
+		local s="${COMP_WORDS[i]}"
+		case "$s" in
+    --*) ;;
+		-*) ;;
+		*) 	cmd="$s"
+			break
+			;;
+		esac
+		i=$((++i))
+	done
+
+	if [[ $i -eq $COMP_CWORD ]]; then
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    case "$cur" in
+      -*)
+        __smithycomp "
+        --arch=
+        --config-file=
+        --disable-group-writable
+        --file-group-name=
+        --help
+        --no-color
+        --software-root="
+        return
+        ;;
+      *)
+        __smithycomp "
+        build
+        deploy
+        help
+        new
+        search"
+        return
+        ;;
+    esac
+		return
+	fi
+
+	# subcommands have their own completion functions
+	case "$cmd" in
+	b|build)  _smithy_build  ;;
+	s|search) _smithy_search ;;
+	new)      _smithy_new    ;;
+	help)     _smithy_help   ;;
+	*)        ;;
+	esac
+}
+
+__smithy_complete_packages ()
+{
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	local packages=$(smithy search --format=name ${cur})
+	COMPREPLY=($(compgen -W "$packages" -- "$cur"))
+}
+
+_smithy_build () {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	local prv="${COMP_WORDS[COMP_CWORD-1]}"
+  case "$prv" in
+  --build-log-name=*)
+    COMPREPLY=($(compgen -f "$cur"))
+    return
+    ;;
+  esac
+	case "$cur" in
+	-*)
+		__smithycomp "
+      --build-log-name=
+      --disable-build-log
+      --dry-run
+      --send-to-stdout"
+		return
+		;;
+	esac
+  __smithy_complete_packages
+}
+
+_smithy_search () {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	local prv="${COMP_WORDS[COMP_CWORD-1]}"
+
+  case "$prv" in
+  --format=*)
+    __smithycomp "path name table csv"
+    return
+    ;;
+  esac
+	case "$cur" in
+	-*)
+		__smithycomp "--format="
+		return
+		;;
+	esac
+}
+
+_smithy_new () {
+	local cur="${COMP_WORDS[COMP_CWORD]}"
+	local prv="${COMP_WORDS[COMP_CWORD-1]}"
+
+  case "$prv" in
+  --tarball=*)
+    COMPREPLY=($(compgen -f "$cur"))
+    return
+    ;;
+  esac
+	case "$cur" in
+	-*)
+		__smithycomp "
+      --dry-run
+      --tarball=
+      --web-description"
+		return
+		;;
+	esac
+}
+
+_smithy_help () {
+  __smithycomp "build search new"
+}
+
+complete -F _smithy smithy
--- etc/autocomplete.zsh
+++ etc/autocomplete.zsh
@@ -1,13 +1,58 @@
-_smith_completion() {
-  if [ -z $2 ] ; then
-    reply=(`bundle exec smith help -c`)
-  else
-    reply=(`bundle exec smith help -c $2`)
-  fi
-}
-compctl -K _smith_completion bundle exec smith
+#compdef smithy
 
-_smith_search_completion() {
-  reply=(`bundle exec smith search --format=name $@`)
+_smithy_all_formulae() {
+	formulae=(`smithy search`) # FIXME _call_program should be used here
 }
-compctl -K _smith_search_completion bundle exec smith search
+
+_smithy_installed_formulae() {
+	installed_formulae=(`smithy list`) # FIXME _call_program should be used here
+}
+
+local -a _1st_arguments
+_1st_arguments=(
+	'new:create a new software build'
+	'build:build an existing package'
+	'search:search existing packages'
+)
+
+local expl
+local -a formulae installed_formulae
+
+_arguments \
+	'(-v)-v[verbose]' \
+	'(--cellar)--cellar[smithy cellar]' \
+	'(--config)--config[smithy configuration]' \
+	'(--env)--env[smithy environment]' \
+	'(--repository)--repository[smithy repository]' \
+	'(--version)--version[version information]' \
+	'(--prefix)--prefix[where smithy lives on this system]' \
+	'(--cache)--cache[smithy cache]' \
+	'*:: :->subcmds' && return 0
+
+if (( CURRENT == 1 )); then
+	_describe -t commands "smithy subcommand" _1st_arguments
+	return
+fi
+
+case "$words[1]" in
+	search|-S)
+		_arguments \
+			'(--macports)--macports[search the macports repository]' \
+			'(--fink)--fink[search the fink repository]' ;;
+	list|ls)
+		_arguments \
+			'(--unsmithyed)--unsmithyed[files in smithy --prefix not controlled by smithy]' \
+			'(--versions)--versions[list all installed versions of a formula]' \
+			'1: :->forms' &&  return 0
+
+			if [[ "$state" == forms ]]; then
+				_smithy_installed_formulae
+				_wanted installed_formulae expl 'installed formulae' compadd -a installed_formulae
+			fi ;;
+	install|home|homepage|log|info|abv|uses|cat|deps|edit|options)
+		_smithy_all_formulae
+		_wanted formulae expl 'all formulae' compadd -a formulae ;;
+	remove|rm|uninstall|unlink|cleanup|link|ln)
+		_smithy_installed_formulae
+		_wanted installed_formulae expl 'installed formulae' compadd -a installed_formulae ;;
+esac
