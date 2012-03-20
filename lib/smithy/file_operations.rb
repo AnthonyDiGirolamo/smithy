125ccbe2ad58b31f1a0fe89fd777bad9858799b1
Anthony DiGirolamo <anthony.digirolamo@gmail.com>
2012-03-20 14:10:05 -0400

    Fixed a bug when using noop in make_group_writable

--- lib/smithy/file_operations.rb
+++ lib/smithy/file_operations.rb
@@ -35,12 +35,14 @@ module Smithy
           command = "chmod -R g+w #{f}"
         else
           command = "chmod g+w #{f}"
-          # Check to see if it's already group writeable
-          # convert the integer to a string in base 8
-          mode = File.stat(f).mode.to_s(8)
-          # check the group bit, convert back to integer
-          group_bit = mode[mode.size-2].to_i
-          return if group_bit == 6 || group_bit == 7
+          unless options[:noop]
+            # Check to see if it's already group writeable
+            # convert the integer to a string in base 8
+            mode = File.stat(f).mode.to_s(8)
+            # check the group bit, convert back to integer
+            group_bit = mode[mode.size-2].to_i
+            return if group_bit == 6 || group_bit == 7
+          end
         end
 
         puts command if options[:verbose]
