ac70b397c2abbd3e7ef7a421556d6a35eb701dff
Anthony DiGirolamo <anthony.digirolamo@gmail.com>
2012-04-05 14:07:33 -0400

    missing steps from module deploy

--- lib/smithy/module_file.rb
+++ lib/smithy/module_file.rb
@@ -71,7 +71,11 @@ module Smithy
       end
 
       notice "Deploying modulefile #{destination}"
+      install_dir = File.join(system_module_path, package.name)
+      FileUtils.mkdir_p install_dir
       FileOperations.install_file module_file, destination, options
+      FileOperations.make_group_writable(install_dir, options.merge(:recursive => true))
+      FileOperations.set_group(install_dir, package.group, options.merge(:recursive => true))
     end
 
   end
