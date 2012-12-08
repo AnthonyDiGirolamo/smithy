82d54173583760afcd2a03f3346924228c36e273
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2012-12-08 14:04:20 -0500

    adding a formula install|list command

--- lib/formulas/openmpi_formula.rb
+++ lib/formulas/openmpi_formula.rb
@@ -1,4 +1,4 @@
-class Openmpi < Formula
+class Openmpi_formula < Formula
 	homepage 'http://www.open-mpi.org'
 	url      'http://www.open-mpi.org/software/ompi/v1.6/downloads/openmpi-1.6.3.tar.bz2'
 end
--- lib/formulas/zlib_formula.rb
+++ lib/formulas/zlib_formula.rb
@@ -1,4 +1,4 @@
-class Zlib < Formula
+class Zlib_formula < Formula
 	homepage 'http://zlib.net'
 	url      'http://zlib.net/zlib-1.2.7.tar.gz'
 end
