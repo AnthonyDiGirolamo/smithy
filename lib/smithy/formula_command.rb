bc65a854ad72dc95cc1cd5c2946cf1c0b6a4b0f4
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-02-01 14:50:02 -0500

    option to clear existing install

--- lib/smithy/formula_command.rb
+++ lib/smithy/formula_command.rb
@@ -70,6 +70,34 @@ module Smithy
         packages.each do |package|
           f = build_formula(package, options[:"formula-name"])
           f.package.create(:formula => true)
+
+          formula_prefix_contents = Dir["#{f.package.prefix}/*"]
+          unless formula_prefix_contents.empty?
+            overwrite = nil
+            while overwrite.nil? do
+              notice_conflict f.package.prefix
+              prompt = Readline.readline(" "*FILE_NOTICE_COLUMNS+"Is not empty, delete contents? (enter \"h\" for help) [ynhq] ")
+              case prompt.downcase
+              when "y"
+                overwrite = true
+              when "n"
+                overwrite = false
+              when "h"
+                indent = " "*FILE_NOTICE_COLUMNS
+                puts indent+"y - yes, delete existing install"
+                puts indent+"n - no, do not overwrite"
+                puts indent+"h - help, show this help"
+                puts indent+"q - exit now"
+              when "q"
+                raise "Abort new formula install"
+              end
+            end
+
+            formula_prefix_contents.each do |f|
+              FileUtils.rm_rf(f)
+            end
+          end
+
           d = DownloadCache.new(f, options[:"formula-name"]).get
           raise "Download failure" unless d
           # f.package.extract(:archive => d, :overwrite => true)
