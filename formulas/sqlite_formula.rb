10668e5b90978dbe9f5d9da008c5237a142d996f
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-02-18 16:03:47 -0500

    added sqlite and subversion formulas

---
+++ formulas/sqlite_formula.rb
@@ -0,0 +1,12 @@
+class SqliteFormula < Formula
+  homepage "http://sqlite.org/"
+  url      "http://sqlite.org/sqlite-autoconf-3071502.tar.gz"
+  version  "3.7.15.2"
+  sha1     "075732562183d560cd46a0d8d08b50bc44e34eac"
+
+  def install
+    module_list
+    system "./configure --prefix=#{prefix}", "--disable-dependency-tracking", "--enable-dynamic-extensions"
+    system "make install"
+  end
+end
---
+++ formulas/subversion_formula.rb
@@ -0,0 +1,25 @@
+class SubversionFormula < Formula
+  homepage "http://subversion.apache.org/"
+  url "http://mirror.cogentco.com/pub/apache/subversion/subversion-1.7.8.tar.bz2"
+  sha1 "12c7d8d5414bba74c9777c4d1dae74f152df63c2"
+
+  version "1.7.8"
+
+  depends_on ["neon", "apr", "apr-util", "sqlite"]
+
+  def install
+    module_list
+
+    system "./configure",
+      "--prefix=#{prefix}",
+      "--with-ssl",
+      "--with-zlib=/usr",
+      "--with-sqlite",
+      "--with-neon=#{neon.prefix}",
+      "--with-apr=#{apr.prefix}",
+      "--with-apr-util=#{apr_util.prefix}",
+		  "--with-sqlite=#{sqlite.prefix}"
+
+    system "make install"
+  end
+end
