bc429f380e3d6b0046213166eaac4a7ca63938fe
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-02-22 13:16:08 -0500

    Comment out existing formula class and spec, start TDD'ing

--- lib/smithy/formula.rb
+++ lib/smithy/formula.rb
@@ -1,132 +1,165 @@
 module Smithy
   class Formula
-    attr_accessor :module_setup, :formula_file_path
-
-    def package
-      @package
+    def self.formula_name
+      self.to_s.underscore.gsub /_formula$/, ""
     end
 
-    def package=(p)
-      @package    = p
-      @name       = @package.name
-      @version    = @package.version
-      @build_name = @package.build_name
-      @prefix     = @package.prefix
-      return @package
+    def self.install
+      raise "no install method implemented"
     end
 
-    def initialize(args = {})
-      self.package = args[:package] if args[:package]
+    # def self.homepage(value = nil)
+    #   @homepage = value unless @homepage
+    #   @homepage
+    # end
 
-      @formula_file_path = args[:path] if args[:path]
+    # def self.url(value = nil)
+    #   @url = value unless @url
+    #   @url
+    # end
 
-      @module_setup = ''
-
-      if ENV['MODULESHOME']
-        @modulecmd = "modulecmd sh"
-        @modulecmd = "#{ENV['MODULESHOME']}/bin/modulecmd sh" if File.exists?("#{ENV['MODULESHOME']}/bin/modulecmd")
-        @module_setup << `#{@module_setup} #{@modulecmd} purge 2>/dev/null`
-        @module_setup << ' '
-        if modules
-          @module_setup << `#{@module_setup} #{@modulecmd} load #{@modules.join(' ')}`
-          @module_setup << ' '
-        end
-      end
-
-      check_dependencies if depends_on
-    end
-
-    def check_dependencies
-      @depends_on = [depends_on] if depends_on.is_a? String
-      missing_packages = []
-      depends_on.each do |package|
-        name, version, build = package.split('/')
-        path = Package.all(:name => name, :version => version, :build => build).first
-        if path
-          p = Package.new(:path => path)
-          new_name = p.name.underscore
-          class_eval %Q{
-            def #{new_name}
-              @#{new_name} = Package.new(:path => "#{path}") if @#{new_name}.nil?
-              @#{new_name}
-            end
-          }
-        else
-          missing_packages << package
-          #TODO build package instead?
-        end
-      end
-
-      unless missing_packages.empty?
-        raise "#{self.class} depends on: #{missing_packages.join(" ")}"
-      end
-    end
-
-    def patch(content)
-      patch_file_name = "patch.diff"
-      File.open(patch_file_name, "w+") do |f|
-        f.write(content)
-      end
-      `patch -p1 <#{patch_file_name}`
-    end
-
-    def system(*args)
-      notice args.join(' ')
-      if args.first == :nomodules
-        args.shift
-        Kernel.system args.join(' ')
-      else
-        Kernel.system @module_setup + args.join(' ')
-      end
-      if $?.exitstatus != 0
-        raise <<-EOF.strip_heredoc
-          The last command exited with status: #{$?.exitstatus}
-            Formula: #{formula_file_path}
-            Build Directory: #{@package.source_directory}
-        EOF
-      end
-    end
-
-    def run_install
-      install
-      notice_success "SUCCESS #{@package.prefix}"
-      return true
-    end
-
-    def module_list
-      if ENV['MODULESHOME']
-        notice "module list"
-        Kernel.system @module_setup + "#{@modulecmd} list 2>&1"
-      end
-    end
-
-    # DSL and instance methods
-
-    %w{depends_on url homepage md5 sha1 sha2 sha256 version name build_name prefix modules modulefile}.each do |attr|
+    %w{ url homepage }.each do |attr|
       class_eval %Q{
-        def self.#{attr}(value = nil, &block)
-          if block_given?
-            @#{attr} = block
-          elsif value
-            @#{attr} = value
-          end
-
+        def self.#{attr}(value = nil)
+          @#{attr} = value unless @#{attr}
           @#{attr}
         end
 
         def #{attr}
-          unless @#{attr}
-            if self.class.#{attr}.is_a?(Proc)
-              @#{attr} = instance_eval(&self.class.#{attr})
-            else
-              @#{attr} = self.class.#{attr}
-            end
-          end
-
-          @#{attr}
+          @#{attr} = self.class.#{attr} unless @#{attr}
         end
       }
     end
+  end
 
-  end #class Formula
+#   class Formula
+#     attr_accessor :module_setup, :formula_file_path
+
+#     def package
+#       @package
+#     end
+
+#     def package=(p)
+#       @package    = p
+#       @name       = @package.name
+#       @version    = @package.version
+#       @build_name = @package.build_name
+#       @prefix     = @package.prefix
+#       return @package
+#     end
+
+#     def initialize(args = {})
+#       self.package = args[:package] if args[:package]
+
+#       @formula_file_path = args[:path] if args[:path]
+
+#       @module_setup = ''
+
+#       if ENV['MODULESHOME']
+#         @modulecmd = "modulecmd sh"
+#         @modulecmd = "#{ENV['MODULESHOME']}/bin/modulecmd sh" if File.exists?("#{ENV['MODULESHOME']}/bin/modulecmd")
+#         @module_setup << `#{@module_setup} #{@modulecmd} purge 2>/dev/null`
+#         @module_setup << ' '
+#         if modules
+#           @module_setup << `#{@module_setup} #{@modulecmd} load #{@modules.join(' ')}`
+#           @module_setup << ' '
+#         end
+#       end
+
+#       check_dependencies if depends_on
+#     end
+
+#     def check_dependencies
+#       @depends_on = [depends_on] if depends_on.is_a? String
+#       missing_packages = []
+#       depends_on.each do |package|
+#         name, version, build = package.split('/')
+#         path = Package.all(:name => name, :version => version, :build => build).first
+#         if path
+#           p = Package.new(:path => path)
+#           new_name = p.name.underscore
+#           class_eval %Q{
+#             def #{new_name}
+#               @#{new_name} = Package.new(:path => "#{path}") if @#{new_name}.nil?
+#               @#{new_name}
+#             end
+#           }
+#         else
+#           missing_packages << package
+#           #TODO build package instead?
+#         end
+#       end
+
+#       unless missing_packages.empty?
+#         raise "#{self.class} depends on: #{missing_packages.join(" ")}"
+#       end
+#     end
+
+#     def patch(content)
+#       patch_file_name = "patch.diff"
+#       File.open(patch_file_name, "w+") do |f|
+#         f.write(content)
+#       end
+#       `patch -p1 <#{patch_file_name}`
+#     end
+
+#     def system(*args)
+#       notice args.join(' ')
+#       if args.first == :nomodules
+#         args.shift
+#         Kernel.system args.join(' ')
+#       else
+#         Kernel.system @module_setup + args.join(' ')
+#       end
+#       if $?.exitstatus != 0
+#         raise <<-EOF.strip_heredoc
+#           The last command exited with status: #{$?.exitstatus}
+#             Formula: #{formula_file_path}
+#             Build Directory: #{@package.source_directory}
+#         EOF
+#       end
+#     end
+
+#     def run_install
+#       install
+#       notice_success "SUCCESS #{@package.prefix}"
+#       return true
+#     end
+
+#     def module_list
+#       if ENV['MODULESHOME']
+#         notice "module list"
+#         Kernel.system @module_setup + "#{@modulecmd} list 2>&1"
+#       end
+#     end
+
+#     # DSL and instance methods
+
+#     %w{depends_on url homepage md5 sha1 sha2 sha256 version name build_name prefix modules modulefile}.each do |attr|
+#       class_eval %Q{
+#         def self.#{attr}(value = nil, &block)
+#           if block_given?
+#             @#{attr} = block
+#           elsif value
+#             @#{attr} = value
+#           end
+
+#           @#{attr}
+#         end
+
+#         def #{attr}
+#           unless @#{attr}
+#             if self.class.#{attr}.is_a?(Proc)
+#               @#{attr} = instance_eval(&self.class.#{attr})
+#             else
+#               @#{attr} = self.class.#{attr}
+#             end
+#           end
+
+#           @#{attr}
+#         end
+#       }
+#     end
+
+#   end #class Formula
 end #module Smithy
--- spec/formula_spec.rb
+++ spec/formula_spec.rb
@@ -1,44 +1,107 @@
-require 'spec_helper'
-
-class Testzlib < Formula
-	homepage 'http://zlib.net'
-	url      'http://zlib.net/zlib-1.2.7.tar.gz'
-	md5      '60df6a37c56e7c1366cca812414f7b85'
-
-	def install
-	end
-end
-
-class Testzlibempty < Formula
-end
+require 'active_support/core_ext/string'
+require 'active_support/inflector'
+require 'smithy'
+include Smithy
 
 describe Formula do
-	context 'with defined values' do
-		subject { Testzlib.new }
-		it { subject.url.should == 'http://zlib.net/zlib-1.2.7.tar.gz' }
-		it { subject.homepage.should == 'http://zlib.net' }
-		it { subject.md5.should == '60df6a37c56e7c1366cca812414f7b85' }
-		it { subject.should respond_to(:install) }
-	end
-
-	context 'with excluded values' do
-		subject { Testzlibempty.new }
-		it { subject.url.should be_nil }
-		it { subject.should_not respond_to(:install) }
-	end
-end
-
-describe FormulaCommand do
-  describe '#list' do
-    it { FormulaCommand.formula_names.should include('zlib') }
-  end
-
-  describe '#build_formula' do
-    subject { FormulaCommand.build_formula('zlib/1.2.7/gnu4.2') }
-    it 'should detect name, version, and prefix' do
-      subject.name.should == 'zlib'
-      subject.version.should == '1.2.7'
-      subject.prefix.should include('zlib/1.2.7/gnu4.2')
+  it "knows it's name" do
+    class TestFormula < Formula
     end
+    TestFormula.formula_name.should == "test"
   end
+
+  it "can run a defined install method" do
+    class TestFormulaWithInstall < Formula
+      def install
+      end
+    end
+    TestFormulaWithInstall.new.should respond_to :install
+  end
+
+  it "raises an error if the install method is not implemented" do
+    class EmptyTestFormula < Formula
+    end
+
+    expect { EmptyTestFormula.new.install }.to raise_error
+  end
+
+  it "has a homepage" do
+    class HomepageTestFormula < Formula
+      homepage "http://rspec.info/"
+    end
+
+    Formula.homepage.should be_nil
+    HomepageTestFormula.homepage.should == "http://rspec.info/"
+  end
+
+  it "has a url" do
+    class UrlTestFormula < Formula
+      url "https://rubygems.org/downloads/rspec-2.12.0.gem"
+    end
+
+    Formula.url.should be_nil
+    UrlTestFormula.url.should == "https://rubygems.org/downloads/rspec-2.12.0.gem"
+  end
+
+  it "can use homepage value" do
+    class HomepageUrlTestFormula < Formula
+      homepage "http://rspec.info/"
+      url homepage
+    end
+
+    HomepageUrlTestFormula.inspect
+    HomepageUrlTestFormula.url.should == "http://rspec.info/"
+  end
+
+  it "passes values to instances" do
+    HomepageUrlTestFormula.url.should == "http://rspec.info/"
+    HomepageUrlTestFormula.new.url.should == "http://rspec.info/"
+  end
+  it "has a hash"
+  it "sets a version"
 end
+
+# require 'spec_helper'
+
+# class Testzlib < Formula
+# 	homepage 'http://zlib.net'
+# 	url      'http://zlib.net/zlib-1.2.7.tar.gz'
+# 	md5      '60df6a37c56e7c1366cca812414f7b85'
+
+# 	def install
+# 	end
+# end
+
+# class Testzlibempty < Formula
+# end
+
+# describe Formula do
+# 	context 'with defined values' do
+# 		subject { Testzlib.new }
+# 		it { subject.url.should == 'http://zlib.net/zlib-1.2.7.tar.gz' }
+# 		it { subject.homepage.should == 'http://zlib.net' }
+# 		it { subject.md5.should == '60df6a37c56e7c1366cca812414f7b85' }
+# 		it { subject.should respond_to(:install) }
+# 	end
+
+# 	context 'with excluded values' do
+# 		subject { Testzlibempty.new }
+# 		it { subject.url.should be_nil }
+# 		it { subject.should_not respond_to(:install) }
+# 	end
+# end
+
+# describe FormulaCommand do
+#   describe '#list' do
+#     it { FormulaCommand.formula_names.should include('zlib') }
+#   end
+
+#   describe '#build_formula' do
+#     subject { FormulaCommand.build_formula('zlib/1.2.7/gnu4.2') }
+#     it 'should detect name, version, and prefix' do
+#       subject.name.should == 'zlib'
+#       subject.version.should == '1.2.7'
+#       subject.prefix.should include('zlib/1.2.7/gnu4.2')
+#     end
+#   end
+# end
