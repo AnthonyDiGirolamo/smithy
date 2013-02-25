ba3631bec6d0d52575542a6e83d3ccb6184e42d0
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2013-02-25 16:16:07 -0500

    cleaning up formula spec

--- spec/formula_spec.rb
+++ spec/formula_spec.rb
@@ -145,71 +145,30 @@ describe Formula do
   end
 
   describe "zlib test formula" do
-    before do
+    before(:all) do
       class ZlibFormula < Formula
         homepage "http://zlib.net"
         url      "http://zlib.net/zlib-1.2.7.tar.gz"
         md5      "60df6a37c56e7c1366cca812414f7b85"
         def install
+          [build_name, prefix]
         end
       end
     end
 
+    let(:p) { stub :name => "zlib",
+                :version => "1.2",
+             :build_name => "macos10.8_gnu4.2",
+                 :prefix => "/tmp/smithy/zlib/1.2/macos10.8_gnu4.2" }
+
     it "takes a package" do
-      p = stub :name => "zlib",
-            :version => "1.2",
-         :build_name => "macos10.8_gnu4.2",
-             :prefix => "/tmp/smithy/zlib/1.2/macos10.8_gnu4.2"
       z = ZlibFormula.new(p)
       z.name.should       == "zlib"
       z.version.should    == "1.2"
       z.build_name.should == "macos10.8_gnu4.2"
       z.prefix.should     == "/tmp/smithy/zlib/1.2/macos10.8_gnu4.2"
+      z.install.should == ["macos10.8_gnu4.2","/tmp/smithy/zlib/1.2/macos10.8_gnu4.2"]
     end
   end
 end
 
-# require 'spec_helper'
-
-# class Testzlib < Formula
-#   homepage 'http://zlib.net'
-#   url      'http://zlib.net/zlib-1.2.7.tar.gz'
-#   md5      '60df6a37c56e7c1366cca812414f7b85'
-
-#   def install
-#   end
-# end
-
-# class Testzlibempty < Formula
-# end
-
-# describe Formula do
-#   context 'with defined values' do
-#     subject { Testzlib.new }
-#     it { subject.url.should == 'http://zlib.net/zlib-1.2.7.tar.gz' }
-#     it { subject.homepage.should == 'http://zlib.net' }
-#     it { subject.md5.should == '60df6a37c56e7c1366cca812414f7b85' }
-#     it { subject.should respond_to(:install) }
-#   end
-
-#   context 'with excluded values' do
-#     subject { Testzlibempty.new }
-#     it { subject.url.should be_nil }
-#     it { subject.should_not respond_to(:install) }
-#   end
-# end
-
-# describe FormulaCommand do
-#   describe '#list' do
-#     it { FormulaCommand.formula_names.should include('zlib') }
-#   end
-
-#   describe '#build_formula' do
-#     subject { FormulaCommand.build_formula('zlib/1.2.7/gnu4.2') }
-#     it 'should detect name, version, and prefix' do
-#       subject.name.should == 'zlib'
-#       subject.version.should == '1.2.7'
-#       subject.prefix.should include('zlib/1.2.7/gnu4.2')
-#     end
-#   end
-# end
