642e5453cba94ba17e06778820386bd87d19754d
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-03-13 09:05:25 -0400

    updated build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bundle exec smithy _doc
+bundle exec ./bin/smithy _doc
 bundle exec ronn --style='toc' man/man1/smithy.1.ronn man/man5/smithyformula.5.ronn
 bundle exec rake rerdoc
-rake -f Rakefile_traveling_ruby package:linux:x86_64
+rake package:linux:x86_64
