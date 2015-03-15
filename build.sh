e2f4a4a09dec877c8e15275f423dc4904e21f64d
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-03-14 22:36:47 -0400

    small fixes to Rakefile and build.sh, version bump to 1.8.0

--- build.sh
+++ build.sh
@@ -2,4 +2,6 @@
 bundle exec ./bin/smithy _doc
 bundle exec ronn --style='toc' man/man1/smithy.1.ronn man/man5/smithyformula.5.ronn
 bundle exec rake rerdoc
-rake package:linux:x86_64
+bundle exec rake package:linux:x86
+bundle exec rake package:linux:x86_64
+bundle exec rake package:osx
--- lib/smithy_version.rb
+++ lib/smithy_version.rb
@@ -36,5 +36,5 @@
 # }}}
 
 module Smithy
-  VERSION = '1.7.0'
+  VERSION = '1.8.0'
 end
